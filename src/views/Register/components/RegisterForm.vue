2154772e3ae69f051a11c8c22f49826810afde0b
EbosetaleseDunjoyin <eboselese@gmail.com>
2023-11-17 13:17:13 +0100

    fix

--- src/views/Register/components/RegisterForm.vue
+++ src/views/Register/components/RegisterForm.vue
@@ -195,7 +195,7 @@ export default {
                 .then(response => {
                     if (!response.ok) {
                         // throw new Error('Network response was not ok');
-                        this.notification("error", "Error", "This email already exists with us");
+                        // this.notification("error", "Error", "This email already exists with us");
                         this.buttonText = "Submit";
                     }
                     return response.json(); // Assuming the response is JSON
